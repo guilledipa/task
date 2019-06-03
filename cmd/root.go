201646ce1d6850f95550a452b7f0b64511cf0564
Guillermo Di Pasquale <guilledipasquale@gmail.com>
2019-06-03 22:00:11 +1000

    Add logic to rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"log"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,4 +11,11 @@ var rootCmd = &cobra.Command{
 	Short: "task is a CLI for managing your TODOs.",
 	Long: `A gophercises excercise #7
 				https://github.com/gophercises/task`,
+}
+
+// Execute will run rootCmd after being called
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("Root cmd: %v.", err)
+	}
 }

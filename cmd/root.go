2c3605872a79ac0981fce2a90522a9de7e1d4bff
Guillermo Di Pasquale <guilledipasquale@gmail.com>
2019-06-03 23:21:58 +1000

    add stuff

--- cmd/root.go
+++ cmd/root.go
@@ -1,10 +1,18 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"proyectos/task/db"
 
 	"github.com/spf13/cobra"
 )
+
+func init() {
+	rootCmd.Flags().StringVarP(&dbPath, "db_path", "d", "~/tasks.db", "Database path")
+	cobra.OnInitialize(initDB)
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "task",
@@ -17,8 +25,14 @@ var dbPath string
 
 // Execute will run rootCmd after being called
 func Execute() {
-	rootCmd.Flags().StringVarP(&dbPath, "db_path", "d", "~/tasks.db", "Database path")
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("Root cmd: %v.", err)
+	}
+}
+
+func initDB() {
+	if err := db.Init(dbPath); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 }
